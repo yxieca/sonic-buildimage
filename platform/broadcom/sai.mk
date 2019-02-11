d65e1d85d580398c099d1043d7fdb1145eba3cdd
Ying Xie <ying.xie@microsoft.com>
2019-02-11 20:32:58 +0000

    [bcm sai] Upgrade Broadcom SAI to version 3.3.4.3m-2
    - Including Broadcom patch for CS7252202.
    
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.3.4.3-1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm_3.3.4.3-1_amd64.deb?sv=2015-04-05&sr=b&sig=eqQ%2BzMqpkcr8mnztwUPfUsmaWL%2Fvj0Cr2hCvALNXLtQ%3D&se=2032-09-26T21%3A45%3A32Z&sp=r"
+BRCM_SAI = libsaibcm_3.3.4.3m-2_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm_3.3.4.3m-2_amd64.deb?sv=2015-04-05&sr=b&sig=hqX0wlXi5FsA%2B%2BwZPuFVUy57duuyw3nDk2wLLGIuwk4%3D&se=2032-10-20T20%3A31%3A28Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.3.4.3-1_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.3.4.3m-2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm-dev_3.3.4.3-1_amd64.deb?sv=2015-04-05&sr=b&sig=v7XeVaxL8H1BDsHUDtyvFIfaWrW9LILA2IEo3YypZSA%3D&se=2032-09-26T21%3A45%3A09Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm-dev_3.3.4.3m-2_amd64.deb?sv=2015-04-05&sr=b&sig=Nbiey%2B4gVq9da293%2FXE%2F7zaRIQEPVVmAGgvNfGHmDHU%3D&se=2032-10-20T20%3A30%3A59Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
