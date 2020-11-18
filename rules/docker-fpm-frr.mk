1127faa34fbc9f46485e60b89f545c97063f77f9
Ying Xie <ying.xie@microsoft.com>
2020-11-18 18:30:09 +0000

    [frr] mount /var/log/frr onto frr docker and configure logrotate
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>

--- rules/docker-fpm-frr.mk
+++ rules/docker-fpm-frr.mk
@@ -23,6 +23,7 @@ SONIC_DOCKER_DBG_IMAGES += $(DOCKER_FPM_FRR_DBG)
 $(DOCKER_FPM_FRR)_CONTAINER_NAME = bgp
 $(DOCKER_FPM_FRR)_RUN_OPT += --privileged -t
 $(DOCKER_FPM_FRR)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
+$(DOCKER_FPM_FRR)_RUN_OPT += -v /var/log/frr:/var/log/frr:rw
 
 $(DOCKER_FPM_FRR)_FILES += $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)
 
